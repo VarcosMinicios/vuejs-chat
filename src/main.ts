152706e120f8629ffe1da313e31a8d18763bef8e
VarcosMinicios <marcos11vini2@outlook.com>
2023-12-31 17:30:44 -0300

    refactor: add axios defaults and change icon lib from quasar

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,19 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import { Quasar, Dialog } from 'quasar'
 import langPt from 'quasar/lang/pt-BR'
 
 import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
-import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
 
 import 'quasar/src/css/index.sass'
+
+axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
 
 const app = createApp(App)
 
